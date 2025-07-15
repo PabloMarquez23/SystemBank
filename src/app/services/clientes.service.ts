f4ac661ad9e11394e550c9c1428ef1f24ded8cce
Andres <andresllivichushca@gmail.com>
2025-07-15 17:17:27 -0500

    reintentos en clientes

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -1,16 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { retry, timeout } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-  private apiUrl = '/api';  // Apunta al backend
+  private apiUrl = '/api';
 
   constructor(private http: HttpClient) {}
 
   getClientes() {
-    return this.http.get<any[]>(`${this.apiUrl}/clientes`);
+    return this.http.get<any[]>(`${this.apiUrl}/clientes`).pipe(
+      retry(3),        // Reintenta 3 veces en caso de error
+      timeout(5000)    // Si no responde en 5 segundos, lanza error
+    );
   }
 
   crearCliente(cliente: { nombre: string, correo: string }) {
@@ -18,24 +22,22 @@ export class ClienteService {
   }
 
   actualizarCliente(id: number, cliente: any) {
-  return this.http.put<any>(`${this.apiUrl}/clientes/${id}`, cliente);
-}
+    return this.http.put<any>(`${this.apiUrl}/clientes/${id}`, cliente);
+  }
 
-eliminarCliente(id: number) {
-  return this.http.delete(`${this.apiUrl}/clientes/${id}`);
-}
+  eliminarCliente(id: number) {
+    return this.http.delete(`${this.apiUrl}/clientes/${id}`);
+  }
 
-crearCuenta(cuenta: { numero_cuenta: string; cliente_id: number; saldo: number }) {
-  return this.http.post('/api/cuentas', cuenta);
-}
+  crearCuenta(cuenta: { numero_cuenta: string; cliente_id: number; saldo: number }) {
+    return this.http.post('/api/cuentas', cuenta);
+  }
 
-getClientePorCedula(cedula: string) {
-  return this.http.get<any>(`/api/clientes/cedula/${cedula}`);
-}
+  getClientePorCedula(cedula: string) {
+    return this.http.get<any>(`/api/clientes/cedula/${cedula}`);
+  }
 
-getCuentaPorCedula(cedula: string) {
-  return this.http.get<any>(`/api/cuentas/cedula/${cedula}`);
-}
-
-
+  getCuentaPorCedula(cedula: string) {
+    return this.http.get<any>(`/api/cuentas/cedula/${cedula}`);
+  }
 }
